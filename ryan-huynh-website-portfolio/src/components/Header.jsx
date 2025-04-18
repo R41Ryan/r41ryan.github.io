5c019d95ec7047d7c2647e223a4ba31e01a4a221
Ryan Huynh <73033285+R41Ryan@users.noreply.github.com>
2025-04-17 19:27:22 -0600

    created basic header

---
+++ ryan-huynh-website-portfolio/src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <div className="header">
+      <div className="header-left-content">
+        <h1>Ryan Huynh</h1>
+        <h2>Software Engineer</h2>
+      </div>
+      <div className="header-right-content navbar">
+        <ul className="navbar-list">
+          
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
