cf0cd2de34bd63fa832064e10dd07c3168440d07
Ryan Huynh <73033285+R41Ryan@users.noreply.github.com>
2025-04-17 19:33:55 -0600

    added more header links

--- website-portfolio/src/components/Header.jsx
+++ website-portfolio/src/components/Header.jsx
@@ -7,7 +7,11 @@ function Header() {
       </div>
       <div className="header-right-content navbar">
         <ul className="navbar-list">
-          
+          <li className="navlink">Home</li>
+          <li className="navlink">Learn About me</li>
+          <li className="navlink">See my Skills</li>
+          <li className="navlink">Check out my projects</li>
+          <li className="navlink">Contact me</li>
         </ul>
       </div>
     </div>
