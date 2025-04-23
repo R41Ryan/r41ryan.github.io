66da242f77768778198afe3e0182e11cb0b1d7f0
Ryan Huynh <73033285+R41Ryan@users.noreply.github.com>
2025-04-22 21:56:40 -0600

    Made header sticky and fade when scrolling down

--- website-portfolio/src/components/Header.jsx
+++ website-portfolio/src/components/Header.jsx
@@ -1,8 +1,33 @@
 import "./Header.css";
+import { useEffect, useRef, useState } from "react";
 
 function Header() {
+
+  const [isHidden, setIsHidden] = useState(false);
+  const pastScrollHeight = useRef(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY || document.documentElement.scrollTop;
+      console.log(scrollTop, pastScrollHeight.current);
+      if (scrollTop > pastScrollHeight.current) {
+        setIsHidden(true);
+      }
+      else if (scrollTop < pastScrollHeight.current) {
+        setIsHidden(false);
+      }
+      pastScrollHeight.current = scrollTop;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [])
+
   return (
-    <header>
+    <header className={isHidden ? "hidden" : ""}>
       <div className="header-left-content navlink">
         <h1>Ryan Huynh</h1>
         <h2>Software Engineer</h2>
